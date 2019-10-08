364f5667fc1380d883c9da9676c1d078e0dd56d7
Levan0612 <levan2578@gmail.com>
2019-10-08 17:57:21 +0700

    test3

---
+++ src/main/java/com/example/websocketdemo/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.example.websocketdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	//đăng ký một websocket endpoint mà các máy khách sẽ sử dụng 
+	//để kết nối với máy chủ websocket của chúng ta.
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
